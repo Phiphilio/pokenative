85e4b52583ce89089764fda3e1fb2e7537a1315f
paoue <grappercosmo@gmail.com>
2024-11-25 20:05:58 +0100

    ajout de notes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+/**
+ * StyleSheet, Text, View sont des bouts de codes que je peux réutiliser,
+ * importer et exporter.
+ * StyleSheet en revanche est un module, un bout de code qui a un rôle purement interne à mon code.
+ * les modules peuvent faire des calculs, faire des requête API, fournir des constantes etc.
+ * les modules peuvent être des objets(comme StyleSheet), des fonctions utilitaires, des classes, des constantes
+ * ou même des combinaisons de tous ces éléments.
+ *
+ * Par contre, Text et View sont des composants.
+ * les composant sont des fonctions ou des classes qui ont pour rôle de générer un élément graphique.
+ * ça peut être un bouton, une image, du texte, etc.
+ * les composants ne sont utilisables que dans le cadre de react-native (et react)
+ * de nos jours on utilise des composants qui sont des fonctions
+ *
+ *
+ */
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "pink",
+    padding: 30,
+  },
+});
