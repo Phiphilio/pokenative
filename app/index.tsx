807bd098d49f97571c718ed7722bb90236b0e905
paoue <grappercosmo@gmail.com>
2024-11-27 11:19:58 +0100

    changement de composant pour le view et ajout du flex

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 /**
  * StyleSheet, Text, View sont des bouts de codes que je peux réutiliser,
@@ -20,21 +21,37 @@ import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href="/about"> à propos</Link>
-    </View>
+      <Link href={{ pathname: "/pokemon/[id]", params: { id: 3 } }}>
+        pokemon 3
+      </Link>
+    </SafeAreaView>
   );
 }
+/**
+ * SafeAreaView est une vue au même titre que View sauf qu'il s'assure qu'il y ai un padding
+ * au dessus et en dessous de l'application pour ne pas que des éléments externes empêchent de voir l'écran.
+ * je parle d'éléments comme la barre qui montre les notifs etc
+ */
 
 const styles = StyleSheet.create({
   /**
    *StyleSheet est un module de type objet.
    là on a fait appel à la methode create qui nous permets de gérer les styles.
-   cette methode prend en paramètre un objet et renvoie un objet
+   cette methode prend en paramètre un objet et renvoie un objet.
+   StyleSheet.create permet d'avoir un meilleur contrôle sur le style et de donner des propiétés
+   qui sont correctent, sinon dans l'idée, j'aurais directement pu créer un objet et le stocker dans
+   styles
    */
   container: {
-    backgroundColor: "darkgreen",
-    padding: 30,
+    backgroundColor: "#FFC0CBAA",
+    flex: 1, // dis au conteneur de prendre tout l'espace disponible
+    /**
+     * ici flex permet de gérer l'occupation de l'espace d'un élément dans son conteneur.
+     * En css, il aurait besoin d'autres propriétés (comme flex-grow, width ou height)
+     *  pour gérer la taille de son élément
+     *  */
   },
 });
