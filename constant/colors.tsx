04d4195e53720099f36d63aaec9c1e0b072ed2e6
paoue <grappercosmo@gmail.com>
2024-11-28 12:21:10 +0100

    ajout des couleurs

---
+++ constant/colors.tsx
@@ -0,0 +1,41 @@
+// l'idée c'est de créer une constante qui contient déjà toutes les couleurs
+// les couleurs sont réparties dans des propriétés light et dark pour les modes du même non
+export const colors = {
+  light: {
+    identity: "#DC0A2D",
+    dark: "#212121",
+    medium: "666666",
+    light: "E0E0E0",
+    background: "EFEFEF",
+    white: "FFFFFF",
+  },
+  dark: {
+    identity: "#185336",
+    dark: "#212121",
+    medium: "666666",
+    light: "E0E0E0",
+    background: "EFEFEF",
+    white: "FFFFFF",
+  },
+  // couleur pour les types de pokemon
+  pokeType: {
+    Bug: "#A7B723",
+    Dark: "#75574C",
+    Dragon: "#7037FF",
+    Electric: "#F9CF30",
+    Fairy: "#E69EAC",
+    Fighting: "#C12239",
+    Fire: "#F57D31",
+    Flying: "#A891EC",
+    Ghost: "#70559B",
+    Normal: "#AAA67F",
+    Grass: "#74CB48",
+    Ground: "#DEC16B",
+    Ice: "#9AD6DF",
+    Poison: "#A43E9E",
+    Psychic: "#FB5584",
+    Rock: "#B69E31",
+    Steel: "#B7B9D0",
+    Water: "#6493EB",
+  },
+};
