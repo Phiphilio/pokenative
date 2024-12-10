020a3509be3a728c1fef593f40420f490e4afbef
paoue <grappercosmo@gmail.com>
2024-12-10 23:31:35 +0100

    ajout du composant Pressable pour faire un effet lorsqu'on clique dessus

--- components/pokemon/pokemonCard.tsx
+++ components/pokemon/pokemonCard.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, ViewStyle, View } from "react-native";
+import { Image, StyleSheet, ViewStyle, View, Pressable } from "react-native";
 import { ThemedText } from "../ThemedText";
 import { Card } from "../card";
 import { useThemeColors } from "@/hooks/useThemeColors";
@@ -11,28 +11,35 @@ type props = {
 export function PokemonCard({ id, name, style }: props) {
   const colors = useThemeColors();
   return (
-    <Card style={[style, styles.card]}>
-      <View
-        style={[styles.boxShadow, { backgroundColor: colors.grayBackground }]}
-      />
-      {/** le fait de mettre la vue correspondant à l'effet de gris au début de la card fait que tous les autres éléments de la card s'affichent par
+    <Pressable
+      android_ripple={{ color: colors.identity, foreground: true }}
+      style={
+        style /**le style qui est transmis au composaant pokemoncard est maintenant ici pour que les tailles restent les mêmes */
+      }
+    >
+      <Card style={styles.card}>
+        <View
+          style={[styles.boxShadow, { backgroundColor: colors.grayBackground }]}
+        />
+        {/** le fait de mettre la vue correspondant à l'effet de gris au début de la card fait que tous les autres éléments de la card s'affichent par
       dessus. pas besoin de jouer avec l'opacité */}
-      <ThemedText
-        style={styles.textNumber}
-        variant="caption"
-        color="grayMedium"
-      >
-        #{id.toString().padStart(3, "0")}
-      </ThemedText>
-      <Image
-        source={{
-          uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
-        }}
-        width={72}
-        height={72}
-      />
-      <ThemedText>{name}</ThemedText>
-    </Card>
+        <ThemedText
+          style={styles.textNumber}
+          variant="caption"
+          color="grayMedium"
+        >
+          #{id.toString().padStart(3, "0")}
+        </ThemedText>
+        <Image
+          source={{
+            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+          }}
+          width={72}
+          height={72}
+        />
+        <ThemedText>{name}</ThemedText>
+      </Card>
+    </Pressable>
   );
 }
 
