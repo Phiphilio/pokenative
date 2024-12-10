2c0d8acfdd138cae12baa75db21894d91030d39c
paoue <grappercosmo@gmail.com>
2024-12-10 19:13:45 +0100

    ajout du composant QueryClientProvider pour englober toutes l'applications afin que le tanstack/react-query fonctionne

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
+const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </QueryClientProvider>
   );
 }
