62df9318b125db0af4a1c3269fd57011962b5460
paoue <grappercosmo@gmail.com>
2024-12-17 14:37:37 +0100

    ajout du lien dynamique et de la propriété asChild pour faire en sorte que Pressable fonctionne comme un lien

--- components/pokemon/pokemonCard.tsx
+++ components/pokemon/pokemonCard.tsx
@@ -2,6 +2,7 @@ import { Image, StyleSheet, ViewStyle, View, Pressable } from "react-native";
 import { ThemedText } from "../ThemedText";
 import { Card } from "../card";
 import { useThemeColors } from "@/hooks/useThemeColors";
+import { Link } from "expo-router";
 
 type props = {
   stylos?: ViewStyle; // correspond à toutes les valeurs que la propriété style peut recevoir.
@@ -11,35 +12,45 @@ type props = {
 export function PokemonCard({ id, name, stylos }: props) {
   const colors = useThemeColors();
   return (
-    <Pressable
-      android_ripple={{ color: colors.identity, foreground: true }}
-      style={
-        stylos /**le style qui est transmis au composaant pokemoncard est maintenant ici pour que les tailles restent les mêmes */
-      }
-    >
-      <Card style={styles.card}>
-        <View
-          style={[styles.boxShadow, { backgroundColor: colors.grayBackground }]}
-        />
-        {/** le fait de mettre la vue correspondant à l'effet de gris au début de la card fait que tous les autres éléments de la card s'affichent par
+    <Link href={{ pathname: "/pokemon/[id]", params: { id: id } }} asChild>
+      {/**
+       * quand je  l'utilise Link avec expo-router , l'élément à l'interieur (élément enfant) est rendu (afficher) avec une balise <a>, d'où le fait qu'on puisse cliquer dessus.
+       * et dans la mesure où j'utilise "asChild", les caractéristiques de la balise <a> sont transmises à l'élément enfant de Link.
+       * C'est comme si au lieu d'avoir <a><pressable/></a> j'ai <pressable/>
+       */}
+      <Pressable
+        android_ripple={{ color: colors.identity, foreground: true }}
+        style={
+          stylos /**le style qui est transmis au composaant pokemoncard est maintenant ici pour que les tailles restent les mêmes */
+        }
+      >
+        <Card style={styles.card}>
+          <View
+            style={[
+              styles.boxShadow,
+              { backgroundColor: colors.grayBackground },
+            ]}
+          />
+          {/** le fait de mettre la vue correspondant à l'effet de gris au début de la card fait que tous les autres éléments de la card s'affichent par
       dessus. pas besoin de jouer avec l'opacité */}
-        <ThemedText
-          style={styles.textNumber}
-          variant="caption"
-          color="grayMedium"
-        >
-          #{id.toString().padStart(3, "0")}
-        </ThemedText>
-        <Image
-          source={{
-            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
-          }}
-          width={72}
-          height={72}
-        />
-        <ThemedText>{name}</ThemedText>
-      </Card>
-    </Pressable>
+          <ThemedText
+            style={styles.textNumber}
+            variant="caption"
+            color="grayMedium"
+          >
+            #{id.toString().padStart(3, "0")}
+          </ThemedText>
+          <Image
+            source={{
+              uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+            }}
+            width={72}
+            height={72}
+          />
+          <ThemedText>{name}</ThemedText>
+        </Card>
+      </Pressable>
+    </Link>
   );
 }
 
