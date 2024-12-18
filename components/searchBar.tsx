6d9aa6345d0459c7084c7c65304ad1ea8d54da89
paoue <grappercosmo@gmail.com>
2024-12-18 18:54:42 +0100

    la hauteur du champ de saisie de TextInput etait trop petite, c'est pour ça que le texte ne s'affichait pas

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,5 +1,6 @@
 import { useThemeColors } from "@/hooks/useThemeColors";
-import { StyleSheet, TextInput } from "react-native";
+import { Image, StyleSheet, TextInput } from "react-native";
+import { Row } from "./row";
 
 type props = {
   text: string;
@@ -10,22 +11,37 @@ export function SearchBar({ text, changeText }: props) {
   // le but de ce composant sera de récupérer la variable d'état et la fonction qui modifie cette variable d'état
   const colors = useThemeColors();
   return (
-    <TextInput
-      style={[styles.searchBar, { backgroundColor: colors.grayWhite }]}
-      onChangeText={changeText} /**
+    <Row style={[styles.searchBar, { backgroundColor: colors.grayWhite }]}>
+      <Image
+        source={require("@/assets/images/icon/search.png")}
+        style={{ height: 16, width: 16, tintColor: colors.identity }}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={changeText} /**
   onChangeText appelle automatique la fonction qui lui est donné en lui donnant en paramètre ce que l'utilisateur a entrée dans le champ de text
   */
-      value={text} // affiche dans le champ de texte la valeur qui lui est donné
-      placeholder="search"
-    />
+        value={text} // affiche dans le champ de texte la valeur qui lui est donné
+        placeholder="search"
+      />
+    </Row>
   );
 }
 
 const styles = StyleSheet.create({
   searchBar: {
+    flex: 1,
     width: 280,
     height: 32,
     borderRadius: 20,
     padding: 8,
+    gap: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 15,
+    height: 40, // la heute posait problème
+    lineHeight: 16,
+    // color: "black",
   },
 });
