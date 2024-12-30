ab20bc7431fc72a1364d3b34b7c2330391d079f2
paoue <grappercosmo@gmail.com>
2024-12-30 15:45:28 +0100

    il faut impérativement que je revois ça

--- hooks/useFetchQuery.tsx
+++ hooks/useFetchQuery.tsx
@@ -1,16 +1,53 @@
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 const endpoint = "https://pokeapi.co/api/v2";
+type API = {
+  "/pokemon?limit=21": {
+    count: number;
+    next: string | null;
+    results: { name: string; url: string }[];
+  };
+  "/pokemon/[id]/": {
+    id: number;
+    name: string;
+    url: string;
+    weight: number;
+    heigh: number;
+    moves: { move: { name: string } }[];
+    stats: {
+      base_stat: number;
+      stats: {
+        name: string;
+      };
+    }[];
+    cries: {
+      latest: string;
+    };
+    types: {
+      type: {
+        name: string;
+      };
+    }[];
+  };
+};
 
-export function useFetchQuery(path: string) {
+export function useFetchQuery<T extends keyof API>(
+  path: T,
+  params?: Record<string, string | number>
+) {
+  const locallUrl = Object.entries(params ?? {}).reduce(
+    (acc, [key, value]) => acc.replaceAll(`[${key}]`, String(value)),
+    path
+  );
+
   /**
    * useQuery prend on paramètre un objet et renverra un objet contenant plusieurs propriétés dont :
    * data, isFetching, isLoading, error etc
    */
   return useQuery({
-    queryKey: [path],
+    queryKey: [locallUrl],
     /**
-     * son rôle est de vérifier si notre path a déjà été utilisé pour une précédente requête.
+     * son rôle est de vérifier si notre locallUrl a déjà été utilisé pour une précédente requête.
      * si c'est le cas, ça veut dire que react-query a déjà stocké dans le cache les résultats de cette requête
      * donc il ne fera plus appel à queryFn
      */ queryFn: async () => {
@@ -20,12 +57,15 @@ export function useFetchQuery(path: string) {
        * de l'objet de retour.
        */
       await wait(1); // fonction qui simule un léger temps d'attente
-      return fetch(endpoint + path).then((r) => r.json());
+      return fetch(endpoint + locallUrl).then((r) => r.json());
     },
   });
 }
 
-export function useInfiniteFetchQuery(path: string) {
+export function useInfiniteFetchQuery<T extends keyof API>(
+  path: T,
+  params?: Record<string, string | number>
+) {
   return useInfiniteQuery({
     queryKey: [path],
     initialPageParam: endpoint + path,
