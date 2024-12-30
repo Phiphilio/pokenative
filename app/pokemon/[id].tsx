f7926f470cbcef3f4a1c3762b30fcac29e27057e
paoue <grappercosmo@gmail.com>
2024-12-30 15:43:31 +0100

    utilisation de useFetchQuery sur lequel je dois revenir

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,13 +1,28 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { ThemedText } from "@/components/ThemedText";
 import { Row } from "@/components/row";
+import { useFetchQuery } from "@/hooks/useFetchQuery";
 
 export default function Pokemon() {
   const params = useLocalSearchParams();
+  const { data } = useFetchQuery("/pokemon/[id]", { id: params.id });
   const colors = useThemeColors();
-  console.log(params);
+
+  console.log(data);
+  const router = useRouter();
+
+  const retourArriere = () => {
+    router.back();
+  };
   /**
    * le hook useLocalSearchParams() me permet de récupérer uniquement l'id
    * dans l'url
@@ -17,12 +32,23 @@ export default function Pokemon() {
       style={[styles.container, { backgroundColor: colors.identity }]}
     >
       <Row style={styles.header} gap={8}>
-        <Image
-          source={require("@/assets/images/icon/arrow_back.png")}
-          style={[styles.arrowBack, { tintColor: colors.grayWhite }]}
-        />
-        <ThemedText variant="headline" style={{ color: colors.grayWhite }}>
+        <Pressable
+          android_ripple={{ color: colors.identity, foreground: true }}
+          onPress={retourArriere}
+        >
+          <Image
+            source={require("@/assets/images/icon/arrow_back.png")}
+            style={[styles.arrowBack, { tintColor: colors.grayWhite }]}
+          />
+        </Pressable>
+        <ThemedText
+          variant="headline"
+          style={[styles.pokemonName, { color: colors.grayWhite }]}
+        >
           {params.name}
+        </ThemedText>
+        <ThemedText variant="subtitle2" style={{ color: colors.grayWhite }}>
+          #{params.id.padStart(3, "0")}
         </ThemedText>
       </Row>
     </SafeAreaView>
@@ -40,9 +66,14 @@ const styles = StyleSheet.create({
   header: {
     padding: 20,
     paddingBottom: 24,
+    justifyContent: "center",
+    alignItems: "center",
   },
   arrowBack: {
     height: 32,
     width: 32,
+  },
+  pokemonName: {
+    paddingRight: 150,
   },
 });
