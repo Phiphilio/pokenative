81d4dd9dd4a94c9dca1b0e9c7211dcef952ed168
paoue <grappercosmo@gmail.com>
2025-01-03 17:41:57 +0100

    dès le moment où des lignes de code risquaient de se répéter dans plusieurs fichiers, on a crée getPokemonArtwork

--- functions/pokemon.tsx
+++ functions/pokemon.tsx
@@ -1,3 +1,8 @@
 export function getPokemonId(url: string): number {
   return parseInt(url.split("/").at(-2)!, 10);
 }
+
+// dès le moment où la même ligne de code risqait de se répéter, il a crée ne fonction pour faire la tâche
+export function getPokemonArtwork(id: number): string {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+}
