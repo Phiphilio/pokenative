138e78e42cf11f90b88251a03ac60cd21da47a00
paoue <grappercosmo@gmail.com>
2025-01-03 19:46:28 +0100

    ajout au niveau de la note

--- functions/pokemon.tsx
+++ functions/pokemon.tsx
@@ -2,7 +2,7 @@ export function getPokemonId(url: string): number {
   return parseInt(url.split("/").at(-2)!, 10);
 }
 
-// dès le moment où la même ligne de code risqait de se répéter, il a crée ne fonction pour faire la tâche
+// dès le moment où la même ligne de code risqait de se répéter, plus précisément dans plusieurs fichiers, il a crée ne fonction pour faire la tâche
 export function getPokemonArtwork(id: number): string {
   return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 }
