e311ea5680d27f640bebf9c3eab93e7eb4ed6202
paoue <grappercosmo@gmail.com>
2025-01-06 15:07:45 +0100

    composant qui affiche les badges de types

--- components/typeBadge.tsx
+++ components/typeBadge.tsx
@@ -1,23 +1,33 @@
-import { StyleSheet, View, ViewProps } from "react-native";
+import { StyleSheet, View, ViewProps, Text } from "react-native";
 import { ThemedText } from "./ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
+import { colors } from "@/constant/colors";
 
 type props = ViewProps & {
   background: string;
+  text: string;
 };
-export function TypeBadge({ background, ...rest }: props) {
-  const colors = useThemeColors();
+export function TypeBadge({ background, text, ...rest }: props) {
   return (
-    <View style={[styles.badge, { backgroundColor: background }]} {...rest}>
-      <ThemedText variant="subtitle3" color="grayWhite" />
+    <View style={[styles.badge, { backgroundColor: background }]}>
+      <ThemedText variant="subtitle3" color="grayWhite" style={styles.text}>
+        {text}
+      </ThemedText>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   badge: {
+    alignItems: "center",
+    justifyContent: "center",
     height: 20,
     width: 46,
-    borderRadius: 20,
+    borderRadius: 10,
+  },
+  text: {
+    // flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
