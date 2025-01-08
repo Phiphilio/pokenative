182f9dcd09d5070a8ce806b78ff3b05df830597c
paoue <grappercosmo@gmail.com>
2025-01-08 18:16:51 +0100

    fin de la mise en place du composant

--- components/pokemonSpec.tsx
+++ components/pokemonSpec.tsx
@@ -1,5 +1,35 @@
-import { View } from "react-native";
+import { Image, StyleSheet, View, ViewProps } from "react-native";
+import { Row } from "./row";
+import { ThemedText } from "./ThemedText";
 
-export function PokemonSpec() {
-  return <View></View>;
+type props = ViewProps & {
+  title: string;
+  description: string;
+  image?: string;
+};
+export function PokemonSpec({
+  title,
+  description,
+  image,
+  style,
+  ...rest
+}: props) {
+  return (
+    <View style={[styles.alignement, style]}>
+      <Row gap={8}>
+        {image && <Image source={image} />}
+        <ThemedText> {title}</ThemedText>
+      </Row>
+      <ThemedText variant="caption" color="grayMedium">
+        {description}
+      </ThemedText>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  alignement: {
+    alignItems: "center",
+    padding: 5,
+  },
+});
