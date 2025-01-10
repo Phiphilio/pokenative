4cb163033f81684f23c60befa5aedea485f70ef2
paoue <grappercosmo@gmail.com>
2025-01-10 20:20:00 +0100

    mise en place d'un composant pour afficher les base stats

---
+++ components/pokemonStats.tsx
@@ -0,0 +1,67 @@
+import { StyleSheet, View, ViewProps } from "react-native";
+import { Row } from "./row";
+import { ThemedText } from "./ThemedText";
+import { useThemeColors } from "@/hooks/useThemeColors";
+
+type props = ViewProps & {
+  name: string;
+  stats: number;
+  color: string;
+};
+export function PokemonStats({ name, stats, color, ...rest }: props) {
+  const colors = useThemeColors();
+  return (
+    <Row>
+      <ThemedText
+        variant="subtitle3"
+        style={[
+          styles.nameSection,
+          { color: color, borderRightColor: colors.grayLight },
+        ]}
+      >
+        {name}
+      </ThemedText>
+      <ThemedText style={styles.statsSection}>
+        {stats.toString().padStart(3, "0")}
+      </ThemedText>
+      <View style={styles.barStats}>
+        <View
+          style={[
+            styles.jaugeBarStats,
+            { width: stats, backgroundColor: color },
+          ]}
+        >
+          <View
+            style={[styles.backgroundBarStats, { backgroundColor: color }]}
+          ></View>
+        </View>
+      </View>
+    </Row>
+  );
+}
+
+const styles = StyleSheet.create({
+  nameSection: {
+    borderRightWidth: 1,
+    paddingRight: 5,
+  },
+  statsSection: {
+    paddingLeft: 5,
+  },
+  barStats: {
+    marginLeft: 10,
+    borderRadius: 20,
+    height: 4,
+    width: 233,
+  },
+  backgroundBarStats: {
+    borderRadius: 40,
+    height: 4,
+    width: 233,
+    opacity: 0.24,
+  },
+  jaugeBarStats: {
+    borderRadius: 40,
+    height: 4,
+  },
+});
